cd32cae0165685f96d892e49e23408431ba1ac33
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-22 19:00:30 -0500

    Initial commit

---
+++ src/main/java/com/example/msbootcoinwallet/MsBootcoinWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.msbootcoinwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcoinWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBootcoinWalletApplication.class, args);
+    }
+
+}
